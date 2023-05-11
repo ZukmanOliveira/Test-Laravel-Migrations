69d2ea7cc995ecd010616130b791a8990468250b
zukman <ronildowod@gmail.com>
2023-05-11 02:02:40 -0300

    task4-fix-task

--- database/migrations/task4/2021_11_09_082155_create_categories_table.php
+++ database/migrations/task4/2021_11_09_082155_create_categories_table.php
@@ -16,6 +16,7 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
             $table->timestamps();
         });
     }
