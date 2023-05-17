526d62d2614b5aadf868d97a5b041d933fa0ebde
zukman <ronildowod@gmail.com>
2023-05-16 22:15:10 -0300

    task-fix

--- database/migrations/task10/2021_11_09_090752_create_countries_table.php
+++ database/migrations/task10/2021_11_09_090752_create_countries_table.php
@@ -14,7 +14,7 @@ class CreateCountriesTable extends Migration
     public function up()
     {
         Schema::create('countries', function (Blueprint $table) {
-            $table->id();
+            $table->id()->nullable();
             $table->string('name');
             $table->timestamps();
         });
